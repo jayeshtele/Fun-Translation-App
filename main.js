0eb64f89b505d54f646bb1732578d0c9348484ac
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-01-08 23:50:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const input = document.getElementById("input");
 const btn = document.querySelector(".btn");
 const output = document.querySelector(".output");
+
+btn.addEventListener("click", ()=>{
+    if(!input.value){
+        alert("Please put your message to Translator Inbox");
+        return;
+    }
+    const task = input.value;
+    output.innerText = task
+    input.value = "";
+})
